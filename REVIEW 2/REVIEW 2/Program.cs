f8314df0314cd5268b848b4d62f43a8560e54d5f
ACT-STUDENT <ACT-STUDENT@LAB3-PC21>
2025-12-10 09:50:12 +0800

    Review

---
+++ REVIEW 2/REVIEW 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REVIEW_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shape Geometry area calculator:");
+            Console.WriteLine("1. Circle");
+            Console.WriteLine("2. Rectangle");
+            Console.Write("Select shape (1 or 2): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+            if (choice == 1)
+            {
+                Circle circle = new Circle();
+                circle.InputShape();
+                circle.InputCircle();
+
+                Console.WriteLine("\n            'OUTPUT'              ");
+                circle.DisplayCircle();
+            }
+            else if (choice == 2)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.InputShape();
+                rectangle.InputRectangle();
+
+                Console.WriteLine("\n            'OUTPUT'              ");
+                rectangle.DisplayRectangle();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
