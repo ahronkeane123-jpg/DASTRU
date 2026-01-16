6e9042eda22723c33cba79eb99a24027549a91e6
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2026-01-16 08:54:17 +0800

    2D array

---
+++ 2D/2D/Heroes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D
+{
+    class Heroes
+    {
+        public string Name { get; set; }
+        public string Power { get; set; }
+        public string Universe { get; set; }
+        public string Alignment { get; set; }
+        public string Team { get; set; }
+
+        public Heroes (string name, string power, string universe, string alignment, string team)
+        {
+            Name = name;
+            Power = power;
+            Universe = universe;
+            Alignment = alignment;
+            Team = team;
+        }
+    }
+}
