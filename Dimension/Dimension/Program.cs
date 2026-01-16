3d756d43415d60745b4d7137e21b841d7d6ce96b
ACT-STUDENT <ACT-STUDENT@LAB3-PC22>
2026-01-16 09:14:23 +0800

    Dimension

---
+++ Dimension/Dimension/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Superhero Information\n         ");
+
+
+            // Creating superheroes
+            Avengers avenger1 = new Avengers(
+                "Captain America",
+                "Avengers: Endgame",
+                "Super strength and leadership",
+                "Team Leader"
+            );
+
+            Avengers avenger2 = new Avengers(
+                "Black Widow",
+                "Avengers",
+                "Expert spy and martial artist",
+                "Strategist"
+            );
+
+            // Displaying details
+            avenger1.DisplayInfo();
+            avenger2.DisplayInfo();
+
+            Console.WriteLine("Thanks for viewing the superhero information.");
+            Console.ReadLine();
+        }
+    }
+}
