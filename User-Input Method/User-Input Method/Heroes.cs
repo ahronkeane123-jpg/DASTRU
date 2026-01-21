6206676070773d6dfac911ab99ce1a619b61ab97
ACT-STUDENT <ACT-STUDENT@LAB3-PC21>
2026-01-21 09:41:51 +0800

    2D User Input

---
+++ User-Input Method/User-Input Method/Heroes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_Method
+{
+    internal class Heroes
+    {
+        public string Name { get; set; }
+        public string Power { get; set; }
+        public string Universe { get; set; }
+        public string Alignment { get; set; }
+        public string Team { get; set; }
+
+        public Heroes(string name, string power, string universe, string alignment, string team)
+        {
+            Name = name;
+            Power = power;
+            Universe = universe;
+            Alignment = alignment;
+            Team = team;
+        }
+    }
+}
