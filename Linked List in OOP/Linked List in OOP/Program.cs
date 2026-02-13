faf9d54b8e8168e10945d0cf1c6e8f0748936b2b
ACT-STUDENT <ACT-STUDENT@LAB3-PC22>
2026-02-13 08:37:17 +0800

    asdasd

---
+++ Linked List in OOP/Linked List in OOP/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_in_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Create something to manage all the heroes
+                HeroManager manager = new HeroManager();
+
+                // This will store the option the user picks from the menu
+                int choice;
+
+                // Keep running the program until the user decides to exit
+                do
+                {
+                    // Display menu and get user choice
+                    choice = UserInput.ShowMenu();
+
+                    // Check what the user selected
+                    switch (choice)
+                    {
+                        case 1:
+                            // Get hero details and insert hero
+                            Hero hero = UserInput.GetHeroDetails();
+                            manager.InsertHero(hero);
+                            break;
+
+                        case 2:
+                            // Ask for hero ID to remove
+                            Console.Write("Enter Hero ID to remove: ");
+                            int id = int.Parse(Console.ReadLine());
+                            manager.RemoveHero(id);
+                            break;
+
+                        case 3:
+                            // Display all heroes
+                            manager.DisplayHeroes();
+                            break;
+
+                        case 4:
+                            // Exit the program
+                            Console.WriteLine("Program exited.");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Try again.");
+                            break;
+                    }
+
+                } while (choice != 4);
+            }
+        }
+    }
+}
