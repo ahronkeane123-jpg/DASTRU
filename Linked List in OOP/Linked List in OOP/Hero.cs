5d941d50121e211cc48b21c8bc6fd7c15c2e4486
ACT-STUDENT <ACT-STUDENT@LAB3-PC22>
2026-02-13 08:37:33 +0800

    Hero

---
+++ Linked List in OOP/Linked List in OOP/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_in_OOP
+{
+    internal class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+
+        public Hero(int id, string name, string power)
+        {
+            Id = id;
+            Name = name;
+            Power = power;
+        }
+    }
+}
